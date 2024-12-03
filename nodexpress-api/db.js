2af54ced383940b9ece8851dd10c0b80cde205bb
freack21 <fixri2104@gmail.com>
2024-12-03 14:45:27 +0700

    fix: db connect closed state

--- nodexpress-api/db.js
+++ nodexpress-api/db.js
@@ -1,18 +1,34 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME, 
-});
+let db;
 
-db.connect((err) => {
+function handleDisconnect() {
+  db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  db.connect((err) => {
     if (err) {
-        console.error("Database connection failed:", err.message);
+      console.error("Error connecting to the database:", err.message);
+      setTimeout(handleDisconnect, 2000);
     } else {
-        console.log("Connected to the database.");
+      console.log("Connected to the database.");
     }
-});
+  });
+
+  db.on("error", (err) => {
+    console.error("Database error:", err.message);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = db;
