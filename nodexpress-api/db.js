db35e19a73ab3968bc847fc0bb2d8d6a78dd8f88
freack21 <fixri2104@gmail.com>
2024-12-05 15:52:20 +0700

    update: db from connect to pool

--- nodexpress-api/db.js
+++ nodexpress-api/db.js
@@ -1,34 +1,13 @@
 const mysql = require("mysql2");
 
-let db;
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-function handleDisconnect() {
-  db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-
-  db.connect((err) => {
-    if (err) {
-      console.error("Error connecting to the database:", err.message);
-      setTimeout(handleDisconnect, 2000);
-    } else {
-      console.log("Connected to the database.");
-    }
-  });
-
-  db.on("error", (err) => {
-    console.error("Database error:", err.message);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
-
-module.exports = db;
+module.exports = pool;
